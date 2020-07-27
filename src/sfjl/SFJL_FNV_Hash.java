48e1f0bfe8f5d43bc7ef98cbd46c5246390f063f
clankill3r <clankill3r@gmail.com>
2020-07-27 20:23:42 +0200

    removed vararg methods after vararg test

--- src/sfjl/SFJL_FNV_Hash.java
+++ src/sfjl/SFJL_FNV_Hash.java
@@ -18,40 +18,55 @@ public class SFJL_FNV_Hash {
 
 // "In the general case, almost any offset_basis will serve so long as it is non-zero."
 //      quote by: - http://www.isthe.com/chongo/tech/comp/fnv/
-// I can't remember how I got to 1083068131, neither I care
+// Note(Doeke): I can't remember how I got to 1083068131, neither I care (I sure did some effort)
 static public final int FNV_START_HASH_32 = 1083068131;
 static public final int FNV_START_HASH_64 = FNV_START_HASH_32;
 
 
 
-
-static public long fnv_hash_64(long... p) {
+static public long fnv_hash_64(long[] p, int length) {
     long h = FNV_START_HASH_64;
-    for (int i = 0; i < p.length; i++ ) {
+    for (int i = 0; i < length; i++ ) {
         h = ( h ^ p[i] ) * 1099511628211L;
     }
     return h;
 }
 
-static public long fnv_hash_56(long... p) {
-    long h = fnv_hash_64(p);
+static public long fnv_hash_64(long[] p) {
+    return fnv_hash_64(p, p.length);
+}
+
+static public long fnv_hash_56(long[] p, int length) {
+    long h = fnv_hash_64(p, length);
     long MASK_56 = (1L<<56)-1;
     return (h>>>56) ^ (h & MASK_56);
 }
 
-static public long fnv_hash_48(long... p) {
-    long h = fnv_hash_64(p);
+static public long fnv_hash_56(long[] p) {
+    return fnv_hash_56(p, p.length);
+}
+
+static public long fnv_hash_48(long[] p, int length) {
+    long h = fnv_hash_64(p, length);
     long MASK_48 = (1L<<48)-1;
     return (h>>>48) ^ (h & MASK_48);
 }
 
-static public long fnv_hash_40(long... p) {
-    long h = fnv_hash_40(p);
+static public long fnv_hash_48(long[] p) {
+    return fnv_hash_48(p, p.length);
+}
+
+static public long fnv_hash_40(long[] p, int length) {
+    long h = fnv_hash_64(p, length);
     long MASK_40 = (1L<<40)-1;
     return (h>>>40) ^ (h & MASK_40);
 }
 
-static public int fnv_hash_32(int... p) {
+static public long fnv_hash_40(long[] p) {
+    return fnv_hash_40(p, p.length);
+}
+
+static public int fnv_hash_32(int[] p, int length) {
     int h = FNV_START_HASH_32;
     for (int i = 0; i < p.length; i++ ) {
         h = ( h ^ p[i] ) * 16777619;
@@ -59,22 +74,38 @@ static public int fnv_hash_32(int... p) {
     return h;
 }
 
-static public int fnv_hash_24(int... p) {
-    int h = fnv_hash_32(p);
+static public int fnv_hash_32(int[] p) {
+    return fnv_hash_32(p, p.length);
+}
+
+static public int fnv_hash_24(int[] p, int length) {
+    int h = fnv_hash_32(p, length);
     int MASK_24 = (1<<24)-1;
     return (h>>>24) ^ (h & MASK_24);
 }
 
-static public int fnv_hash_16(int... p) {
-    int h = fnv_hash_32(p);
+static public int fnv_hash_24(int[] p) {
+    return fnv_hash_24(p, p.length);
+}
+
+static public int fnv_hash_16(int[] p, int length) {
+    int h = fnv_hash_32(p, length);
     int MASK_16 = (1<<16)-1;
     return (h>>>16) ^ (h & MASK_16);
 }
 
-static public int fnv_hash_8(int... p) {
-    int h = fnv_hash_32(p);
+static public int fnv_hash_16(int[] p) {
+    return fnv_hash_16(p, p.length);
+}
+
+static public int fnv_hash_8(int[] p, int length) {
+    int h = fnv_hash_32(p, length);
     int MASK_8 = (1<<8)-1;
     return (h>>>8) ^ (h & MASK_8);
+}
+
+static public int fnv_hash_8(int[] p) {
+    return fnv_hash_8(p, p.length);
 }
 
 static public long fnv_hash_64(String s) {
